9cc447e2c864db12d6322b753ee0813ebb40d8e2
Barry Hanna <barryhanna@users.noreply.github.com>
2022-10-09 21:04:36 +0100

    Move content into modules

---
+++ src/about.js
@@ -0,0 +1,8 @@
+function render(rootEl) {
+  rootEl.innerHTML = `<div id="about" data-tab-content>
+			<h2>About</h2>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta possimus laboriosam excepturi deserunt totam commodi dolore sequi, animi magni est repellat et fugit nisi natus.</p>
+		</div>`;
+}
+
+export default render;
---
+++ src/home.js
@@ -0,0 +1,7 @@
+export default function render(rootEl) {
+  rootEl.innerHTML = `<div id="home" class="active" data-tab-content>
+			<h2>Home</h2>
+			<img src="restaurant.jpg" alt="">
+			<p>Welcome to Tasty Bytes</p>
+		</div>`;
+}
